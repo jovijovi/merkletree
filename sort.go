836c855540c8bf738fe91cbe1da6b1c70d3cca18
jovijovi <mageyul@hotmail.com>
2021-07-29 10:35:53 +0800

    fix: check if receiver is nil

--- sort.go
+++ sort.go
@@ -4,6 +4,23 @@ import (
 	"bytes"
 )
 
-func (obj *Leaves) Len() int           { return len(*obj) }
-func (obj *Leaves) Less(i, j int) bool { return bytes.Compare((*obj)[i].Hash, (*obj)[j].Hash) == -1 }
-func (obj *Leaves) Swap(i, j int)      { (*obj)[i], (*obj)[j] = (*obj)[j], (*obj)[i] }
+func (obj *Leaves) Len() int {
+	if obj == nil {
+		return 0
+	}
+	return len(*obj)
+}
+
+func (obj *Leaves) Less(i, j int) bool {
+	if obj == nil {
+		return false
+	}
+	return bytes.Compare((*obj)[i].Hash, (*obj)[j].Hash) == -1
+}
+
+func (obj *Leaves) Swap(i, j int) {
+	if obj == nil {
+		return
+	}
+	(*obj)[i], (*obj)[j] = (*obj)[j], (*obj)[i]
+}
